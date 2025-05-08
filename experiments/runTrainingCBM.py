fe2fa29d093b531b2b5eaab48c955ddcae5ee456
yassine oueslati <ouesly@usi.ch>
2025-05-08 16:44:43 +0000

    CBM work, testing interventions

--- experiments/runTrainingCBM.py
+++ experiments/runTrainingCBM.py
@@ -2,8 +2,10 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..')))
 from configs.diffusion_config import TrainingConfig
-from models.UNet2DWithCBM import UNet2DWithCBM 
-from models.CBM import CBM
+#from models.UNet2DWithCBM import UNet2DWithCBM
+from models.UNet2DWithCBM_new import UNet2DWithCBM
+from models.CBM import CBM_new
+
 from data.dataloader import get_dataloader
 from train.trainCBM import train_loop
 
@@ -15,8 +17,10 @@ import torch
 
 
 def run():
+    model_name = sys.argv[1]
+
     config = TrainingConfig()
-    model = UNet2DWithCBM(config,CBM)
+    model = UNet2DWithCBM(config,CBM_new)
     dataloader = get_dataloader(config,concept=config.concept)
     noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
     optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
@@ -26,7 +30,7 @@ def run():
         num_training_steps=len(dataloader) * config.num_epochs,
     )
 
-    train_loop(config, model, noise_scheduler, optimizer, dataloader, lr_scheduler)
+    train_loop(config, model, noise_scheduler, optimizer, dataloader, lr_scheduler,model_name=model_name)
 
 if __name__ == "__main__":
     run()
